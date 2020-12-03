14e98cc9b633a2a16a69409e0895defc127ae49b
seanpollen <57369255+seanpollen@users.noreply.github.com>
2020-12-02 19:27:17 -0500

    Add files via upload

---
+++ findlocationmatches.h
@@ -0,0 +1,32 @@
+#ifndef FINDLOCATIONMATCHES_H
+#define FINDLOCATIONMATCHES_H
+
+#include <QWidget>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class findLocationMatches;
+}
+QT_END_NAMESPACE
+
+class findLocationMatches : public QWidget
+{
+    Q_OBJECT
+
+public:
+    findLocationMatches(QWidget *parent = nullptr);
+    ~findLocationMatches();
+
+private slots:
+    void on_findMatches_clicked();
+
+    void on_logout_clicked();
+
+    void changeOccur();
+
+private:
+    Ui::findLocationMatches *ui;
+};
+
+#endif // FINDLOCATIONMATCHES_H
+
